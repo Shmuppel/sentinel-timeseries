cb7c20a09c069da6a97d6bd75f99d09f090fd930
MasterHydro <sot.kechagias@gmail.com>
2022-06-13 12:57:09 +0200

    Second change

--- s1-indexes.py
+++ s1-indexes.py
@@ -1,9 +1,23 @@
 """
-This is Sentinel 1 image processing for calculating indexes in order to find out pooling with radar!
+Author: Sotiris Kechagias (MasterHydro)
+Title : This is Sentinel 1 image processing for calculating indexes in order to find out pooling with radar
+Starting Date : 2022/06/13 (Monday)
 """
 
+#                       TO DO
 # 1. Find Sentinel-2 cloud free images with clear pooling.
 # 2. Clip the water pixel with a `Water Index(es)`.
 # 3. Check the same areas/pixes for the dB values.
 # 4. Use an average calculation to create `water pixel index` in
 #    the following format: min <= value <= max
+
+
+#               INSTRUCTIONS / PARTS
+# Import Sentinel-2 pooling pixels/images/raster
+
+# Select the equivalents for Sentinel-1
+
+# Create a list/table with min-max (dB)
+
+# Calculate and export the indexes
+
