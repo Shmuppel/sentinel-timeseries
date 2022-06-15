0bd5e618ce7227e4d33cf782910615faca33dad1
SoriavU <soriavanuffelen@gmail.com>
2022-06-15 09:50:10 +0200

    Make vv/vh ratio

--- S1_RGB.py
+++ S1_RGB.py
@@ -15,6 +15,7 @@ with ZipFile('E:\ACt\S1A_IW_GRDH_1SDV_20210823T172521_20210823T172546_039360_04A
 ##Open TIFF files using gdal
 import gdal
 import os
+import numpy as np
 dirname = 'temp_tiff/S1A_IW_GRDH_1SDV_20210823T172521_20210823T172546_039360_04A618_4894.SAFE/measurement'
 vh_name = 's1a-iw-grd-vh-20210823t172521-20210823t172546-039360-04a618-002.tiff'
 vv_name = 's1a-iw-grd-vv-20210823t172521-20210823t172546-039360-04a618-001.tiff'
@@ -23,19 +24,24 @@ vv_backscatter = gdal.Open(os.path.join(dirname, vv_name))
 data_vh = vh_backscatter.ReadAsArray()
 data_vv = vv_backscatter.ReadAsArray()
 
+#Replace 0 with nan
+#data_vh_nan = np.where(data_vh==0, np.nan, data_vh)
+#data_vv_nan = np.where(data_vv==0, np.nan, data_vv)
+
 ##Convert from backscatter to dB
 import numpy as np
-vv_dB = np.log10(data_vv)
-vh_dB = np.log10(data_vh)
+vv_dB = np.log10(data_vv, out=np.zeros_like(data_vv, dtype='float32'), where=(data_vv!=0))
+vh_dB = np.log10(data_vh, out=np.zeros_like(data_vh, dtype='float32'), where=(data_vh!=0))
 #Check if it does something (yes!)
 vh_dB_max = np.max(vh_dB)
 vh_dB_min = np.min(vh_dB)
-
-
+vv_dB_max = np.max(vv_dB)
+vv_dB_min = np.min(vv_dB)
 
 ## Calculate vv-vh ratio
-#vv_vh_ratio = vv_dB - vh_dB
-#vv_vh_ratio_min = np.min(vv_vh_ratio)
+vv_vh_ratio = vv_dB - vh_dB
+vv_vh_ratio_min = np.min(vv_vh_ratio)
+vv_vh_ratio_max = np.max(vv_vh_ratio)
 
 #Open area of interest and crop vv and vh images
 import geopandas as gpd
