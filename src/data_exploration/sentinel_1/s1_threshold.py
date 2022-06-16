5b6931241413fea53aa9ef19236de8748963f226
MsterHydro <sot.kechagias@gmail.com>
2022-06-16 11:09:36 +0200

    new branch

---
+++ src/data_exploration/sentinel_1/s1_threshold.py
@@ -0,0 +1,12 @@
+"""
+Date: Thursday 2022/06/16
+Authors: Soria & Sotiris
+Title: Calculate threshold (dB) based classification of pooling
+"""
+
+#           INSTRUCTIONS
+# 1. Select a b-box (potential flooded parcel) [e.g. 10 parcels]
+# 2. Find the min/max/stdv
+# 3. Do that for at least 3 different images
+# 4. Come up with threshold
+
