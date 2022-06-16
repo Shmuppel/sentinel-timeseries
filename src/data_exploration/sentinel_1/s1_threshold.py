b67fc7ea1261553500cedaae5adc56def7a7c82a
MsterHydro <sot.kechagias@gmail.com>
2022-06-16 12:49:16 +0200

    basic skeleton

--- src/data_exploration/sentinel_1/s1_threshold.py
+++ src/data_exploration/sentinel_1/s1_threshold.py
@@ -10,3 +10,36 @@ Title: Calculate threshold (dB) based classification of pooling
 # 3. Do that for at least 3 different images
 # 4. Come up with threshold
 
+#           Import packages
+from osgeo import gdal
+from rasterstats import zonal_stats
+import rasterio
+import numpy
+import fiona
+import geopandas as gpd
+import matplotlib.pyplot as plt
+import rasterio.plot as rplt
+
+#           Import Data
+S1A_20200218_VV = rasterio.open("...")
+S1A_20210619_VV = "..."
+S1A_20211022_VV = "..."
+aoi = "..."
+
+#           Creating B-Box
+
+# B-Box 1
+
+# B-Box 2
+
+# B-Box 3
+
+
+#           Zonal Statistics /rasterstats/
+zonal_stats("...shp...", "...tif...", stats="count min median mean max")
+
+#           Calculate Averages
+
+
+#           Export results
+
