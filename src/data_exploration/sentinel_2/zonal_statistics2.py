7738c0505a775328d55f6ea65ec9b5bb90d49138
Fenna van Tillo <fennavantillo@gmail.com>
2022-06-20 15:11:25 +0200

    Added function get_parcels added rasterstats for individual parcels

--- src/data_exploration/sentinel_2/zonal_statistics2.py
+++ src/data_exploration/sentinel_2/zonal_statistics2.py
@@ -1,17 +1,47 @@
+#load libraries
 from rasterstats import zonal_stats
+from rasterio.plot import show
 from src.util import *
-from src.data_exploration.sentinel_2.s2_indices import main
+from src.data_exploration.sentinel_2.s2_indices import calculate_indices
+import matplotlib.pyplot as plt
+import shapely
+import geopandas as gpd
+import pandas as pd
 
-parcel_path = 'resources/study_area/AOI_BRP_WGS84.geojson'
-parcels = get_study_area(parcel_path)
 
-# polygon_path = 'resources/study_area/Polygon.geojson'
-# aoi_polygon = get_study_area(polygon_path)
+#import shapefile
 
-ndwi_gao, ndwi_mcfeeters, mndwi_xu = main()
+#load parcels
+parcelpath = r"C:\Projects\pooling-detection\resources\study_area\BRP_AOI_RDNew.geojson"
+#parcelsall = get_study_area(parcelpath)
+parcelsall = get_parcels(parcelpath)
+#parcelsall = gpd.read_file(parcelpath)
 
-stats1 = zonal_stats(parcels, ndwi_gao, stats='mean')
+# Polygon into geojson
+transformer = pyproj.Transformer.from_crs(
+    pyproj.CRS('EPSG:28992'),  # Assuming the study / crop area is in RD New
+    pyproj.CRS('EPSG:32631'),  # The CRS as specified in the image
+    always_xy=True).transform
+#parcels = transform(transformer, parcelsall)
 
-stats2 = zonal_stats()
+new_parcels = [transform(transformer, shape(parcel['geometry'])) for parcel in parcelsall['features']]
 
-stats3 = zonal_stats()
+
+#load indice maps
+ndwi_gao, ndwi_mcfeeters, mndwi_xu, geometry_transform = calculate_indices()
+
+
+#do some calculations with zonal_stats
+masked_band = np.ma.array(ndwi_gao, mask=(ndwi_gao.mask), dtype=np.float32, fill_value=-999)
+masked_filled = masked_band.filled()
+
+#parcel_json = json.dumps(shapely.geometry.mapping(new_parcels))
+#zonal_stats(parcel_json)
+#['{"type": "Polygon", "coordinates": [[[684827.2392268141, 5884044.476651217], [684829.3855335615, 5884042.766186684], ...]
+
+gao_stats = zonal_stats([shapely.geometry.mapping(parcel) for parcel in new_parcels], masked_filled, affine = geometry_transform, nodata = -999)
+
+print(gao_stats)
+#show(ndwi_mcfeeters, title='NDWI mcfeeters', cmap='gist_ncar')
+
+
