7ef7410f17558858057e073b084504b6dc901958
MasterHydro <sot.kechagias@gmail.com>
2022-06-20 12:40:58 +0200

    Works but can not export image

--- src/data_exploration/sentinel_1/S1_Dry_Dry_Wet.py
+++ src/data_exploration/sentinel_1/S1_Dry_Dry_Wet.py
@@ -4,11 +4,14 @@ Credit: Reverse-engineered code of Soria's s1_rgb_edited.py
 Date: 2022/06/20 | Author: Sotiris
 """
 # Import Packages
+import os
 import numpy as np
+from matplotlib import pyplot as plt
 from osgeo import gdal
 from zipfile import ZipFile
 from rasterio.plot import show
 from src.util import get_study_area, get_image_data
+
 
 #  Extract .tif from zip
 def extract_tif_from_zip(zipfile_name, output_loc):
@@ -44,15 +47,15 @@ def min_max_norm(band):
     return (band - band_min) / (band_max - band_min)
 
 # Create Stack Array
-def stack_arrays(Dry, Dry, Wet):
+def stack_arrays(Dry, Wet):
     s1_rgb = np.stack((Dry, Dry, Wet))
     print('Stack rgb done')
     return s1_rgb
 
 
 def main():
-    zip_name_dry = 'S1A_IW_GRDH_1SDV_20210823T172521_20210823T172546_039360_04A618_4894.zip'
-    zip_name_wet = '...'
+    zip_name_dry = 'resources/S1A_IW_GRDH_1SDV_20211017T171716_20211017T171741_040162_04C1B1_6C50.zip'
+    zip_name_wet = 'resources/S1A_IW_GRDH_1SDV_20211022T172522_20211022T172547_040235_04C439_981A.zip'
     output_dir = 'src/data_exploration/sentinel_1/temp_tiff'
 
     # Extract tif files from a zip file
@@ -61,8 +64,8 @@ def main():
 
     # Open with gdal
     # vv_backscatter = gdal.Open('src/data_exploration/sentinel_1/temp_tiff/S1A_IW_GRDH_1SDV_20210823T172521_20210823T172546_039360_04A618_4894.SAFE/measurement/s1a-iw-grd-vv-20210823t172521-20210823t172546-039360-04a618-001.tiff')
-    vh_backscatter_dry = gdal.Open('src/data_exploration/sentinel_1/temp_tiff/S1A_IW_GRDH_1SDV_20210823T172521_20210823T172546_039360_04A618_4894.SAFE/measurement/s1a-iw-grd-vh-20210823t172521-20210823t172546-039360-04a618-002.tiff')
-    vh_backscatter_wet = gdal.Open('src/data_exploration/sentinel_1/temp_tiff/S1A_IW_GRDH_1SDV_20210823T172521_20210823T172546_039360_04A618_4894.SAFE/measurement/s1a-iw-grd-vh-20210823t172521-20210823t172546-039360-04a618-002.tiff')
+    vh_backscatter_dry = gdal.Open('src/data_exploration/sentinel_1/temp_tiff/S1A_IW_GRDH_1SDV_20211017T171716_20211017T171741_040162_04C1B1_6C50.SAFE/measurement/s1a-iw-grd-vh-20211017t171716-20211017t171741-040162-04c1b1-002.tiff')
+    vh_backscatter_wet = gdal.Open('src/data_exploration/sentinel_1/temp_tiff/S1A_IW_GRDH_1SDV_20211022T172522_20211022T172547_040235_04C439_981A.SAFE/measurement/s1a-iw-grd-vh-20211022t172522-20211022t172547-040235-04c439-002.tiff')
 
     # Warp to epsg 4326
     # vv_warped = gdal.Warp('src/data_exploration/sentinel_1/temp_tiff/vv_warp.tiff', vv_backscatter, dstSRS="EPSG:4326")
@@ -77,7 +80,7 @@ def main():
     # get image data from warped tif files
     # vv = get_image_data("src/data_exploration/sentinel_1/temp_tiff/vv_warp.tiff", study_area)
     vh_dry = get_image_data("src/data_exploration/sentinel_1/temp_tiff/vh_warp_dry.tiff", study_area) # Dry
-    vh_wet = get_image_data("src/data_exploration/sentinel_1/temp_tiff/vh_warp_wet.tiff", study_area) # Wet
+    vh_wet = get_image_data("src/data_exploration/sentinel_1/temp_tiff/vh_warp_wet.tiff", study_area, vh_dry.shape) # Wet
     print('Image data study area acquired')
 
     # convert arrays to decibels and then normalize
@@ -86,11 +89,14 @@ def main():
     vh_db_norm_dry = min_max_norm(vh_db_dry) # Dry
     vh_db_norm_wet = min_max_norm(vh_db_wet) # Wet
     print('Normalise the three bands')
-
+    # breakpoint()
     # stack VH, VH, Dry becomes a ndarray
-    s1_rgb = stack_arrays(vh_db_norm_dry, vh_db_norm_dry, vh_db_norm_wet) # (Dry, Dry, Wet)
+    s1_rgb = stack_arrays(vh_db_norm_dry, vh_db_norm_wet) # (Dry, Dry, Wet)
     show(s1_rgb)
+    plt.savefig('test.png', dpi=300)
 
 
+
+os.chdir('C:\Projects\pooling-detection')
 if __name__ == '__main__':
     main()
