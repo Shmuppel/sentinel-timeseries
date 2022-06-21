620a135d6e264b1dfa80b59e6b78427ad535b042
MsterHydro <sot.kechagias@gmail.com>
2022-06-21 12:29:30 +0200

    Implement functions for preprocessing

--- src/data_exploration/sentinel_1/s1_threshold.py
+++ src/data_exploration/sentinel_1/s1_threshold.py
@@ -12,17 +12,17 @@ Title: Calculate threshold (dB) based classification of pooling
 
 #           Import packages
 import os, rasterio, numpy, fiona, statistics
+import numpy as np
 import geopandas as gpd
 from osgeo import gdal
 from rasterstats import zonal_stats
 from zipfile import ZipFile
-from src.util import get_study_area, get_parcels
+from src.util import get_study_area, get_image_data
 
 #           Sed directory
 os.chdir(r'C:\Projects\pooling-detection')
 
 
-# Extract .tif from zip
 def extract_tif_from_zip(zipfile_name, output_loc):
     # Extract all the tiff files from a specified zip folder
     with ZipFile(zipfile_name, 'r') as zipObj:
@@ -32,6 +32,26 @@ def extract_tif_from_zip(zipfile_name, output_loc):
                 # Extract a single file from zip
                 zipObj.extract(fileName, output_loc)
                 print('The TIFF file is extracted in temp_tiff')
+
+
+def warp_tif_files(im1, im2, im3):
+    # Warp the tif files and safe to new tif
+    output_raster_im1 = "src/data_exploration/sentinel_1/temp_tiff/im1_warp.tif"
+    output_raster_im2 = "src/data_exploration/sentinel_1/temp_tiff/im2_warp.tif"
+    output_raster_im3 = "src/data_exploration/sentinel_1/temp_tiff/im3_warp.tif"
+    im1_w = gdal.Warp(output_raster_im1, im1, dstSRS="EPSG:4326")
+    im2_w = gdal.Warp(output_raster_im2, im2, dstSRS="EPSG:4326")
+    im3_w = gdal.Warp(output_raster_im3, im3, dstSRS="EPSG:4326")
+    print('Files are warped')
+    return im1_w, im2_w, im3_w
+
+
+def convert_to_decibel(vh_warp):
+    # Convert to dB
+    vh_db = np.log10(vh_warp, out=np.zeros_like(vh_warp, dtype='float32'), where=(vh_warp != 0))
+    vh_db = vh_db.astype('float16')
+    print('Files are converted to dB')
+    return vh_db
 
 
 #           Import Data
@@ -56,20 +76,33 @@ S1A_20211022_VH = gdal.Open('src/data_exploration/sentinel_1/temp_tiff'
                             '/S1A_IW_GRDH_1SDV_20211022T172522_20211022T172547_040235_04C439_981A.SAFE/measurement'
                             '/s1a-iw-grd-vh-20211022t172522-20211022t172547-040235-04c439-002.tiff')
 # Vectors
-aoi = get_study_area("resources/study_area/Polygon_WGS84.geojson")  # With function from util.py
+aoi = get_study_area("resources/study_area/Polygon.geojson")  # With function from util.py
 parcels = gpd.read_file("resources/study_area/AOI_BRP_WGS84.geojson")
+
+#                           Pre-Process
+S1A_20200218_VH_warp, S1A_20210619_VH_warp, S1A_20211022_VH_warp = warp_tif_files(S1A_20200218_VH,
+                                                                                  S1A_20210619_VH,
+                                                                                  S1A_20211022_VH)
+S1A_20200218_VH_crop = get_image_data("src/data_exploration/sentinel_1/temp_tiff/im1_warp.tif", aoi)
+S1A_20200218_VH_db = convert_to_decibel(S1A_20200218_VH_crop)
+S1A_20210619_VH_crop = get_image_data("src/data_exploration/sentinel_1/temp_tiff/im2_warp.tif", aoi)
+S1A_20210619_VH_db = convert_to_decibel(S1A_20210619_VH_crop)
+S1A_20211022_VH_crop = get_image_data("src/data_exploration/sentinel_1/temp_tiff/im3_warp.tif", aoi)
+S1A_20211022_VH_db = convert_to_decibel(S1A_20211022_VH_crop)
 
 #           1. Creating B-Box
 bb_1 = parcels[parcels.OBJECTID_1 == 1079]  # 2019-06-17
-bb_2 = parcels[parcels.OBJECTID_1 == 562]   # 2019-06-22
-bb_3 = parcels[parcels.OBJECTID_1 == 121]   # 2020-03-26
+bb_2 = parcels[parcels.OBJECTID_1 == 562]  # 2019-06-22
+bb_3 = parcels[parcels.OBJECTID_1 == 121]  # 2020-03-26
 bb_4 = parcels[parcels.OBJECTID_1 == 1037]  # 2021-10-24
 
 # %%
 #           2. Zonal Statistics /rasterstats/
 # for loop
-bb1_stats = zonal_stats([bb_1, bb_2, bb_3, bb_4], "...tif...", stats="count min median mean max std")
+bb1_stats = zonal_stats([bb_1, bb_2, bb_3, bb_4], S1A_20200218_VH_db, stats="count min median mean max std")
 
+
+# %%
 #           3. Calculate Averages
 averages = statistics.mean(var1, var2, var3)
 
