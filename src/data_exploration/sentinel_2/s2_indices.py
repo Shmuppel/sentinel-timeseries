856979fa022ebaa34451fd8d23af30418d43ead5
Fenna van Tillo <fennavantillo@gmail.com>
2022-06-22 13:46:32 +0200

    Plotted percentile of Gao, McFeeters and MNDWI, Xu

--- src/data_exploration/sentinel_2/s2_indices.py
+++ src/data_exploration/sentinel_2/s2_indices.py
@@ -5,13 +5,13 @@ from src.util import *
 def calculate_indices():
     aoi_json = get_study_area('resources/study_area/Polygon.geojson')
     # load green band
-    green_image = "resources/images/IMG_DATA/T31UFU_20210823T105031_B03.jp2"
+    green_image = r"C:\Projects\pooling-detection\resources\images\T31UFU_20210823T105031_B03.jp2"
     green_array, geometry_transform = get_image_data(green_image, aoi_json)
     # # load NIR Band
-    nir_image = "resources/images/IMG_DATA/T31UFU_20210823T105031_B08.jp2"
+    nir_image = r"C:\Projects\pooling-detection\resources\images\T31UFU_20210823T105031_B08.jp2"
     nir_array,_ = get_image_data(nir_image, aoi_json)
     # load SWIR Data
-    swir_image = "resources/images/IMG_DATA/T31UFU_20210823T105031_B11.jp2"
+    swir_image = r"C:\Projects\pooling-detection\resources\images\T31UFU_20210823T105031_B11.jp2"
     swir_array,_ = get_image_data(swir_image, aoi_json, resample=green_array.shape)
 
     ndwi_gao = calculate_normalized_index(nir_array, swir_array)
--- src/data_exploration/sentinel_2/zonal_statistics2.py
+++ src/data_exploration/sentinel_2/zonal_statistics2.py
@@ -31,24 +31,73 @@ new_parcels = [transform(transformer, shape(parcel['geometry'])) for parcel in p
 ndwi_gao, ndwi_mcfeeters, mndwi_xu, geometry_transform = calculate_indices()
 
 
-#do some calculations with zonal_stats
+#do some calculations with zonal_stats for NDWI Gao index
 masked_band = np.ma.array(ndwi_gao, mask=(ndwi_gao.mask), dtype=np.float32, fill_value=-999)
 masked_filled = masked_band.filled()
 
-#parcel_json = json.dumps(shapely.geometry.mapping(new_parcels))
-#zonal_stats(parcel_json)
-#['{"type": "Polygon", "coordinates": [[[684827.2392268141, 5884044.476651217], [684829.3855335615, 5884042.766186684], ...]
-
-gao_stats = zonal_stats([shapely.geometry.mapping(parcel) for parcel in new_parcels], masked_filled, affine = geometry_transform, nodata = -999)
+gao_stats = zonal_stats([shapely.geometry.mapping(parcel) for parcel in new_parcels], masked_filled, affine = geometry_transform, nodata = -999, stats='percentile_95')
 
 print(gao_stats)
+
+fig, ax = plt.subplots(figsize = (12, 8))
+perc = [stats['percentile_95'] for stats in gao_stats]
+geoparcels['gao_percentile_95'] = perc
+geoparcels.plot(column='gao_percentile_95',ax=ax, cmap='Spectral', legend=True)
+ax.set_title("Gao - percentile_95", fontsize=30)
+plt.show()
+
+#do some calculations with zonal_stats for NDWI mcfeeters
+masked_band2 = np.ma.array(ndwi_mcfeeters, mask=(ndwi_mcfeeters.mask), dtype=np.float32, fill_value=-999)
+masked_filled2 = masked_band.filled()
+
+mcfeeters_stats = zonal_stats([shapely.geometry.mapping(parcel) for parcel in new_parcels], masked_filled2, affine = geometry_transform, nodata = -999, stats='percentile_95')
+
+print(mcfeeters_stats)
+
+fig, ax = plt.subplots(figsize = (12, 8))
+
+percmcfeeters = [stats['percentile_95'] for stats in mcfeeters_stats]
+geoparcels['feeters_percentile_95'] = percmcfeeters
+geoparcels.plot(column='feeters_percentile_95',ax=ax, cmap='Spectral', legend=True)
+ax.set_title("McFeeters - percentile_95", fontsize=30)
+plt.show()
+#do some calculations with zonal_stats for NDWI mndwi Xu
+masked_band3 = np.ma.array(mndwi_xu, mask=(mndwi_xu.mask), dtype=np.float32, fill_value=-999)
+masked_filled3 = masked_band3.filled()
+
+mndwi_stats = zonal_stats([shapely.geometry.mapping(parcel) for parcel in new_parcels], masked_filled3, affine = geometry_transform, nodata = -999, stats='percentile_95')
+
+print(mndwi_stats)
+
+fig, ax = plt.subplots(figsize = (12, 8))
+
+perc = [stats['percentile_95'] for stats in mndwi_stats]
+geoparcels['mndwi_percentile_95'] = perc
+geoparcels.plot(column='mndwi_percentile_95',ax=ax, cmap='Spectral', legend=True)
+ax.set_title("MNDWI, Xu - percentile_95", fontsize=30)
+plt.show()
 #show(ndwi_mcfeeters, title='NDWI mcfeeters', cmap='gist_ncar')
 
 #geopandas dataframe get count, min, max, mean
+'''
+perc = [stats['percentile_95'] for stats in gao_stats]
+geoparcels['percentile_95'] = perc
+geoparcels.plot(column='percentile_95', cmap='Spectral', legend=True)
+plt.show()
+
 
 counts = [stats['count'] for stats in gao_stats]
 geoparcels['counts'] = counts
-
-
-geoparcels.plot(column='counts', legend=True)
+geoparcels.plot(column='counts',cmap='prism', legend=True)
 plt.show()
+
+max = [stats['max'] for stats in gao_stats]
+geoparcels['max'] = max
+geoparcels.plot(column='max',cmap='hot', legend=True)
+plt.show()
+
+mean = [stats['mean'] for stats in gao_stats]
+geoparcels['mean'] = mean
+geoparcels.plot(column='mean', cmap='hot', legend=True)
+plt.show()
+'''
