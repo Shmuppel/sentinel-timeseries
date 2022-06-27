d283ae9120dfbb4346394fdf7f26152ab4e121ea
Fenna van Tillo <fennavantillo@gmail.com>
2022-06-27 12:30:38 +0200

    add sentinel2 pooling hotspot processing

--- src/sentinel-timeseries/process_sentinel2.py
+++ src/sentinel-timeseries/process_sentinel2.py
@@ -1,10 +1,11 @@
 from datetime import date
 from indices import MNDWI_XU
 from rasterstats import zonal_stats
-
+import matplotlib.pyplot as plt
 
 def process_sentinel2(api, parcels):
-    sentinel2_products = api.get_sentinel2_products(date(2022, 1, 1), date(2022, 2, 27))
+    sentinel2_products = api.get_sentinel2_products(date(2022, 1, 1), date(2022, 3, 31))
+    parcels = parcels.assign(stat_count=0, stat_mean=0, stat_max=0, stat_percentile95=0)
     for product in sentinel2_products:
         # ndwi_gao = NDWI_GAO(product)
         # ndwi_mcfeeter = NDWI_MCFEETER(product)
@@ -16,6 +17,28 @@ def process_sentinel2(api, parcels):
             mndwi_xu.calculate().filled(),
             affine=affine_transform,
             nodata=-999,
-            stats=['count', 'max', 'mean', 'percentile_95', 'nodata']
-        )
-        breakpoint()
+            stats=['count', 'max', 'mean', 'percentile_95', 'nodata'])
+
+
+        stat_count, stat_mean, stat_max, stat_percentile95 = [], [], [], []
+        # if there are no pixels we replace the statistics with 0
+        # this should only be done, because we are summing statistics over multiple images
+        for stat in stats:
+            stat_count.append(stat['count'])
+            stat_mean.append(stat['mean'] if stat['mean'] else 0)
+            stat_max.append(stat['max'] if stat['max'] else 0)
+            stat_percentile95.append(stat['percentile_95'] if stat['percentile_95'] else 0)
+
+        parcels['stat_count'] += stat_count
+        parcels['stat_mean'] += stat_mean
+        parcels['stat_max'] += stat_max
+        parcels['stat_percentile95'] += stat_percentile95
+
+    parcels['stat_percentile95'] = parcels['stat_percentile95'] / len(sentinel2_products)
+
+
+    fig, ax = plt.subplots(figsize=(12, 8))
+
+    parcels.plot(column='stat_percentile95', legend=True, cmap='Spectral', ax=ax)
+
+    plt.show()
--- src/sentinel-timeseries/waterlogging-detection.py
+++ src/sentinel-timeseries/waterlogging-detection.py
@@ -12,18 +12,18 @@ from process_sentinel2 import process_sentinel2
 def main():
     load_dotenv()
 
-    study_area = get_polygon_from_geojson('../resources/study_area/Polygon_WGS84.geojson')
-    parcels = gpd.read_file('../resources/study_area/AOI_BRP_WGS84.geojson')
+    study_area = get_polygon_from_geojson('../../resources/study_area/Polygon_WGS84.geojson')
+    parcels = gpd.read_file('../../resources/study_area/AOI_BRP_WGS84.geojson')
 
     api = SentinelTimeseriesAPI(
         username=os.getenv('COPERNICUS_HUB_USERNAME'),
         password=os.getenv('COPERNICUS_HUB_PASSWORD'),
         aoi=study_area,
         warp=CRS('EPSG:4326'),
-        working_directory='../resources/images'
+        working_directory='../../resources/images'
     )
     #process_sentinel1_rgb(api, parcels)
-    #process_sentinel2(api, parcels)
+    process_sentinel2(api, parcels)
 
 
 if __name__ == '__main__':
